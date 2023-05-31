8179047462260eb73316864ab8c636dd87919b12
soype <phenestroza@gmail.com>
2023-05-31 00:11:45 -0300

    Finished basic layout

--- src/components/PLP/Products/ProductsList.js
+++ src/components/PLP/Products/ProductsList.js
@@ -3,15 +3,31 @@ import products from "../../../db/products";
 import stockPrice from "../../../db/stock-price";
 
 function ProductsList() {
-  const renderItem = () => {
-    products.map((item, index) => {
+
+  let productsImg = []
+
+  const generateImg = (index) => {
+    products.forEach(item => {
+      productsImg.push(item.image)
+    })
+    return productsImg[index];
+  }
+
+  return (
+    <div className="products">
+      <div className="products-subtitle">
+        <h2>Popular</h2>
+        <p className="see-all">See All</p>
+      </div>
+      <div className="products-container" id="products">
+        {products.map((item, index) => {
       return (
-        <div className="item">
-          <a className="item-title" href="/127-ModeloEspecial">
+        <div className="item" key={index}>
+          <a className="item-title" href={`${item.id}-${item.brand}`}>
             {item.brand}
           </a>
           <div className="img-container">
-            <img src={item.image} />
+            <img src={generateImg(index)} />
           </div>
           <div className="item-menu">
             <div className="item-price">$ 28.65</div>
@@ -22,16 +38,8 @@ function ProductsList() {
           </div>
         </div>
       );
-    });
-  };
-
-  return (
-    <div className="products">
-      <div className="products-subtitle">
-        <h2>Popular</h2>
-        <p className="see-all">See All</p>
+    })}
       </div>
-      <div className="products-container" id="products"></div>
     </div>
   );
 }
